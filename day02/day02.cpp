10732372b7b2009c09611f14248ba544d282fa02
ddulle3 <ddulle3@gatech.edu>
2020-01-27 20:27:41 -0500

    refactor var names to align with code challenge terminology.

--- day02/day02.cpp
+++ day02/day02.cpp
@@ -10,61 +10,63 @@ void intcode_it(std::vector<int> *vec, int *val_zero_idx) {
     std::vector<int> input = *vec;
 
     for (std::vector<int>::iterator it = input.begin(); it != input.end(); it++) {
-        int op_code = *it;
+        int opcode = *it;
 
-        if (op_code == 99) {
+        if (opcode == 99) {
             *val_zero_idx = input[0];
             return;
         }
-        // kind of want to put an else here, with val1/val2/str_idx inside
+        // kind of want to put an else here,
         // as a way of checking that I don't throw an index out of bounds error
+        // or another type of error,
         // but maybe not needed since I know data is always good
+        //
 
-        int pos_val1 = *(++it);
-        int pos_val2 = *(++it);
-        int store_idx = *(++it);
+        int val1_addr = *(++it);
+        int val2_addr = *(++it);
+        int result_addr = *(++it);
         int result;
 
-        if (op_code == 1) {
-            result = add(input[pos_val1], input[pos_val2]);
+        if (opcode == 1) {
+            result = add(input[val1_addr], input[val2_addr]);
         }
 
-        if (op_code == 2) {
-            result = mult(input[pos_val1], input[pos_val2]);
+        if (opcode == 2) {
+            result = mult(input[val1_addr], input[val2_addr]);
         }
 
-        input[store_idx] = result;
+        input[result_addr] = result;
     }
 }
 
-void intcode_i(std::vector<int> *vec, int *val_zero_idx) {
+void intcode_i(std::vector<int> *vec, int *output) {
     std::vector<int> input = *vec;
 
-    for (int i = 0; i < input.size(); i+=4) {
-        int op_code = input[i];
+    for (int ip = 0; ip < input.size(); ip+=4) {
+        int opcode = input[ip];
 
-        if (op_code == 99) {
-            *val_zero_idx = input[0];
+        if (opcode == 99) {
+            *output = input[0];
             return;
         }
         // kind of want to put an else here, with val1/val2/str_idx inside
         // as a way of checking that I don't throw an index out of bounds error
         // but maybe not needed since I know data is always good
 
-        int pos_val1 = input[i + 1];
-        int pos_val2 = input[i + 2];
-        int store_idx = input[i + 3];
+        int val1_addr = input[ip + 1];  // param 1
+        int val2_addr = input[ip + 2];  // param 2
+        int result_addr = input[ip + 3];  // param 3
         int result;
 
-        if (op_code == 1) {
-            result = add(input[pos_val1], input[pos_val2]);
+        if (opcode == 1) {
+            result = add(input[val1_addr], input[val2_addr]);
         }
 
-        if (op_code == 2) {
-            result = mult(input[pos_val1], input[pos_val2]);
+        if (opcode == 2) {
+            result = mult(input[val1_addr], input[val2_addr]);
         }
 
-        input[store_idx] = result;
+        input[result_addr] = result;
     }
 }
 void parseFileToVec(std::string input_path,
@@ -100,10 +102,10 @@ inline int mult(int val1, int val2) {
 
 
 void unit_test_one() {
-    std::vector<int> parsed_input{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50};
-    int val_zero_pos;
-    intcode_i(&parsed_input, &val_zero_pos);
-    std::cout << "Result in index zero is: " << val_zero_pos << std::endl;
+    std::vector<int> input{1, 9, 10, 3, 2, 3, 11, 0, 99, 30, 40, 50};
+    int program_output;
+    intcode_i(&input, &program_output);
+    std::cout << "Result in memory address zero is: " << program_output << std::endl;
     std::cout << "Result expected is: 3500" << std::endl;
 }
 
@@ -117,9 +119,9 @@ void part_a_result() {
 
     if (success) {
         std::cout << "File parsed." << std::endl;
-        int pos_zero_after;
-        intcode_it(&parsed_input, &pos_zero_after);
-        std::cout << "Result in index zero is: " << pos_zero_after << std::endl;
+        int program_output;
+        intcode_i(&parsed_input, &program_output);
+        std::cout << "Result in memory address zero is: " << program_output << std::endl;
     } else {
         std::cout << "Error parsing file." <<std::endl;
     }
